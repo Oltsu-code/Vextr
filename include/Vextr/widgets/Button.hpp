b2eb6c430ab2661bacc1be5adf8d9cf34eb0c0ac
Oltsu-code <123672634+Oltsu-code@users.noreply.github.com>
2026-04-10 18:51:41 +0300

    fix: clear button for now

--- include/Vextr/widgets/Button.hpp
+++ include/Vextr/widgets/Button.hpp
@@ -4,22 +4,7 @@
 
 namespace vextr::widgets {
 
-class Button : public core::Widget{
-public:
-    Button() = default;
-    explicit Button(const std::string& text) : text(text) {}
-
-    void setText(const std::string& t) { text = t; }
-    const std::string& getText() const { return text; }
-
-    core::Size measure(int availW, int availH) override;
-    void render(backend::Buffer& buf) override;
-
-    bool isFocusable() const override { return true; }
-    bool onEvent(const core::Event& e) override;
-
-private:
-    std::string text;
+class Button{
 };
 
 } // vextr::widgets
