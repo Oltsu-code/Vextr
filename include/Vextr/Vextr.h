26f752da3ea76bff92acdff94bbfee9641655a33
Oltsu-code <123672634+Oltsu-code@users.noreply.github.com>
2026-04-10 23:51:08 +0300

    feat: add Vextr.h

--- include/Vextr/Vextr.h
+++ include/Vextr/Vextr.h
@@ -1,5 +1,17 @@
 #pragma once
 
-namespace vextr {
+#include <Vextr/App.hpp>
 
-}
+#include <Vextr/core/Widget.hpp>
+#include <Vextr/core/Event.hpp>
+#include <Vextr/core/Style.hpp>
+#include <Vextr/core/Geometry.hpp>
+
+#include <Vextr/widgets/Button.hpp>
+#include <Vextr/widgets/Label.hpp>
+#include <Vextr/widgets/Input.hpp>
+#include <Vextr/widgets/Container.hpp>
+#include <Vextr/widgets/Navigator.hpp>
+
+#include <Vextr/layout/StackLayout.hpp>
+#include <Vextr/layout/GridLayout.hpp>
